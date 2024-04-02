52d43fd3430ed01a8e6e2aadec4422ed14ec176e
yoshin04 <taigaby0411@gmail.com>
2024-04-02 15:42:04 +0900

    db定義

--- api/db/db.go
+++ api/db/db.go
@@ -2,26 +2,37 @@ package db
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func NewDB() *gorm.DB {
+	var db *gorm.DB
+	var err error
+
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
+	dbName := os.Getenv("DB_DATABASE")
 	dbHost := os.Getenv("DB_HOST")
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("データベースへの接続に失敗しました")
+	// リトライロジック
+	for i := 0; i < 5; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			fmt.Println("データベースに接続しました")
+			return db
+		}
+		fmt.Println("データベースへの接続に失敗しました。リトライします...")
+		time.Sleep(5 * time.Second) // 5秒待機
 	}
 
-	fmt.Println("データベースに接続しました")
-	return db
+	log.Panic("データベースへの接続に最終的に失敗しました: ", err, "dsn: ", dsn)
+	return nil
 }
 
 func CloseDB(db *gorm.DB) {
--- api/main.go
+++ api/main.go
@@ -3,12 +3,27 @@ package main
 import (
 	"app/controller"
 	"app/db"
+	"app/db/models"
 	"app/router"
 	queryService "app/usecase/queries"
+	"fmt"
 )
 
 func main() {
-	db.NewDB()
+	dbConn := db.NewDB()
+
+	defer func() {
+		db.CloseDB(dbConn)
+		fmt.Println("Successfully Migrated and DB Connection Closed")
+	}()
+
+	// マイグレーションの実行
+	err := dbConn.AutoMigrate(&models.User{}, &models.LinkAbility{}, &models.MSCard{}, &models.PLCard{}, &models.TacticalCard{}, &models.GameDeck{})
+	if err != nil {
+		fmt.Println("マイグレーションに失敗しました:", err)
+		return
+	}
+
 	plCardQueryService := queryService.NewPlCardQueryService()
 	plCardController := controller.NewPlCardController(plCardQueryService)
 	e := router.NewRouter(plCardController)
