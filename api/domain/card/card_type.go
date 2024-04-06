0981ffb93e00908d604141304ef5bf09e7b84e14
yoshin04 <taigaby0411@gmail.com>
2024-04-06 21:01:15 +0900

    MS Card CSVインポート実装

--- api/domain/card/card_type.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-var CardType = map[string]string{
-	"Control":      "CONTROL",      // 制圧
-	"Defense":      "DEFENSE",      // 防御
-	"Annihilation": "ANNIHILATION", // 殲滅
-}
-
-// test
--- api/domain/card/link_ability.go
+++ api/domain/card/link_ability.go
@@ -7,11 +7,52 @@ type LinkAbility struct {
 }
 
 var CardEffects = map[string]string{
-	"MeleeAttackSmallUp":   "MELEE_ATTACK_SMALL_UP",   //近接攻撃力小アップ
-	"MeleeAttackMediumUp":  "MELEE_ATTACK_MEDIUM_UP",  //近距離攻撃力中アップ
-	"RangedAttackSmallUp":  "RANGED_ATTACK_SMALL_UP",  //遠距離攻撃力小アップ
-	"RangedAttackMediumUp": "RANGED_ATTACK_MEDIUM_UP", //遠距離攻撃力中アップ
-	"MobilitySmallUp":      "MOBILITY_SMALL_UP",       // 機動力小アップ
-	"HpSmallUp":            "HP_SMALL_UP",             // HP小アップ
-	"TransformationTactic": "TRANS_FORMATION_TACTIC",  // 変形戦術
+	"MeleeAttackSmallUp":             "MELEE_ATTACK_SMALL_UP",               //近接攻撃力小アップ
+	"MeleeAttackMediumUp":            "MELEE_ATTACK_MEDIUM_UP",              //近距離攻撃力中アップ
+	"MeleeAttackAndHpSmallUp":        "MELEE_ATTACK_AND_HP_SMALL_UP",        //近距離攻撃力、HP小アップ
+	"RangedAttackSmallUp":            "RANGED_ATTACK_SMALL_UP",              //遠距離攻撃力小アップ
+	"RangedAttackMediumUp":           "RANGED_ATTACK_MEDIUM_UP",             //遠距離攻撃力中アップ
+	"MeleeAndRangedAttackSmallUp":    "MELEE_AND_RANGED_ATTACK_SMALL_UP",    //近遠攻撃力小アップ
+	"RangedAttackAndHpSmallUp":       "RANGED_ATTACK_AND_HP_SMALL_UP",       //遠距離攻撃力、HP小アップ
+	"MobilitySmallUp":                "MOBILITY_SMALL_UP",                   // 機動力小アップ
+	"MobilityAndRangedAttackSmallUp": "MOBILITY_AND_RANGED_ATTACK_SMALL_UP", // 機動力、遠距離攻撃力小アップ
+	"MobilityAndMeleeAttackSmallUp":  "MOBILITY_AND_MELEE_ATTACK_SMALL_UP",  // 機動力、近距離攻撃力小アップ
+	"HpSmallUp":                      "HP_SMALL_UP",                         // HP小アップ
+	"MobilityAndHpSmallUp":           "MOBILITY_AND_HP_SMALL_UP",            // 機動力、HP小アップ
+	"TransformationTactic":           "TRANS_FORMATION_TACTIC",              // 変形戦術
+}
+
+func CsvTextConvertToLinkAbilityEffects(csvText string) string {
+	switch csvText {
+	case "近攻":
+		return CardEffects["MeleeAttackSmallUp"]
+	case "近攻[中↑]":
+		return CardEffects["MeleeAttackMediumUp"]
+	case "遠攻":
+		return CardEffects["RangedAttackSmallUp"]
+	case "遠攻[中↑]":
+		return CardEffects["RangedAttackMediumUp"]
+	case "遠/近", "遠攻/近攻", "遠攻/近攻,作戦":
+		return CardEffects["MeleeAndRangedAttackSmallUp"]
+	case "近攻/HP":
+		return CardEffects["MeleeAttackAndHpSmallUp"]
+	case "機動":
+		return CardEffects["MobilitySmallUp"]
+	case "機動/遠攻":
+		return CardEffects["MobilityAndRangedAttackSmallUp"]
+	case "機動/近攻":
+		return CardEffects["MobilityAndMeleeAttackSmallUp"]
+	case "HP":
+		return CardEffects["HpSmallUp"]
+	case "機動/HP":
+		return CardEffects["MobilityAndHpSmallUp"]
+	case "遠攻/HP":
+		return CardEffects["RangedAttackAndHpSmallUp"]
+	case "[変形戦術]":
+		return CardEffects["TransformationTactic"]
+	case "[秘められた力]":
+		return ""
+	default:
+		return ""
+	}
 }
