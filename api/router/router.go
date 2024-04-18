a5f5c20447bdd8cc60bfe2d713a8ad3365c0c973
yoshin04 <taigaby0411@gmail.com>
2024-04-18 20:48:57 +0900

    作戦カード一覧取得API実装

--- api/router/router.go
+++ api/router/router.go
@@ -22,6 +22,7 @@ func NewRouter(plc controller.IPlCardController, msc controller.IMsCardControlle
 	e.GET("/ms-cards", msc.FindMany)
 	e.POST("/ms-cards/import-csv", msc.ImportCsv, middleware.CSVImportAuthMiddleware)
 
+	e.GET("/tactical-cards", tsc.FindAll)
 	e.POST("/tactical-cards/import-csv", tsc.ImportCsv, middleware.CSVImportAuthMiddleware)
 
 	return e
